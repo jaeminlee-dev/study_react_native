80b89b564f37f22fa6b7c16a90cf7fd37d0d5754
jaemin72 <ljm005302@gmail.com>
2022-12-21 12:43:18 +0900

    rn의 레이아웃 시스템을 알아보자
    컴포넌트는 flexbox 알고리즘을 사용하여 자식의 레이아웃을 지정할 수 있습니다. Flexbox는 다양한 스크린 크기에서 일관된 레이아웃을 제공하도록 설계되었습니다.
    일반적으로 레이아웃을 만들기 위해 flexDirection, alignItems 및 justifyContent등을 사용합니다.

--- App.js
+++ App.js
@@ -1,66 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <Text style={styles.text}>hello! I made a RN app</Text>
-      {/* 상태표시줄 */}
+    <View style={{flex: 1}}>
+      <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+      <View style={{flex: 2, backgroundColor: 'tomato'}} />
+      <View style={{flex: 5, backgroundColor: 'blue'}} />
+      <View style={{flex: 1, backgroundColor: 'orange'}} />
+      <View style={{width: 50, height: 50, backgroundColor: 'orange'}} />
     </View>
   );
-};
-
-//style sheet란
-//react native에서는 css를 사용할 수 없다.
-//대신 style sheet를 사용한다.
-//style sheet는 css와 비슷하게 생겼지만, css와는 다르게 사용한다.
-//style sheet는 javascript의 객체이다.
-//style sheet는 javascript의 객체이기 때문에, javascript의 변수를 사용할 수 있다.
-//또한 자동 완성 기능을 사용할 수 있다.
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 20,
-    color: 'red',
-  },
-});
+}
 
 export default App;
