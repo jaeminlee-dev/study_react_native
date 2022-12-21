fdf18de53246bcf51441296b4717b0d30b02acbc
jaemin72 <ljm005302@gmail.com>
2022-12-21 13:28:59 +0900

    Dimensions, scrollview, pagination

--- App.js
+++ App.js
@@ -1,16 +1,69 @@
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {View} from 'react-native';
-
+import {View, Text, StyleSheet, ScrollView, Dimensions} from 'react-native';
+const {width, height} = Dimensions.get('window');
 function App() {
   return (
-    <View style={{flex: 1}}>
-      <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-      <View style={{flex: 2, backgroundColor: 'tomato'}} />
-      <View style={{flex: 5, backgroundColor: 'blue'}} />
-      <View style={{flex: 1, backgroundColor: 'orange'}} />
-      <View style={{width: 50, height: 50, backgroundColor: 'orange'}} />
+    <View style={styles.container}>
+      <StatusBar style="dark"></StatusBar>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>hello</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.tempature}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.tempature}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.tempature}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.tempature}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'tomato',
+  },
+  city: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  cityName: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  weather: {},
+  day: {
+    justifyContent: 'center',
+    width: width,
+  },
+  tempature: {
+    marginTop: 20,
+    fontSize: 178,
+    textAlign: 'center',
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+    textAlign: 'center',
+  },
+});
